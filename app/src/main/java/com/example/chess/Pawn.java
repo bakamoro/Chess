ea54bcb8cec59768e38706810bd61226bbc357d2
Bakamoro <binyaminkenig@gmail.com>
2023-05-14 13:30:31 +0300

    adding document to code

--- app/src/main/java/com/example/chess/Pawn.java
+++ app/src/main/java/com/example/chess/Pawn.java
@@ -3,9 +3,22 @@ package com.example.chess;
 import static com.example.chess.ChessDelegate.green_square;
 
 public class Pawn extends ChessPiece{
+
+    /**
+     * The constructor
+     * @param col - The piece location column.
+     * @param row - The piece location row.
+     * @param player - The color of the piece.
+     * @param resId - The ID of his image.
+     */
     public Pawn(int col, int row, ChessPlayer player, int resId) {
         super(col, row, player,ChessRank.PAWN, resId);
     }
+
+    /**
+     * set green_square to be all the optional movements of the pawn.
+     * @param color - the pawn's color.
+     */
     public void moveOption(String color){
         int i = 0;
         if(color.equals("black")){
