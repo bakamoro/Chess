28a3e1e26c450c10dae6451812c652991557ee9a
Bakamoro <binyaminkenig@gmail.com>
2023-05-14 13:12:46 +0300

    adding document to code

--- app/src/main/java/com/example/chess/Green_Square.java
+++ app/src/main/java/com/example/chess/Green_Square.java
@@ -1,7 +1,14 @@
 package com.example.chess;
 
 public class Green_Square {
-    int col,row;
+    int col//the square's column.
+            ,row;//the square's row.
+
+    /**
+     * the Green_Square's constructor.
+     * @param col - the square's column.
+     * @param row - the square's row.
+     */
     public Green_Square(int col, int row){
         this.col = col;
         this.row = row;
