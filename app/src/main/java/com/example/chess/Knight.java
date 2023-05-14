6c3bd621d848c0d4b21e593a230c5e51a51f1c29
Bakamoro <binyaminkenig@gmail.com>
2023-05-14 13:32:09 +0300

    adding document to code

--- app/src/main/java/com/example/chess/Knight.java
+++ app/src/main/java/com/example/chess/Knight.java
@@ -3,9 +3,21 @@ package com.example.chess;
 import static com.example.chess.ChessDelegate.green_square;
 
 public class Knight extends ChessPiece{
+
+    /**
+     * The constructor
+     * @param col - The piece location column.
+     * @param row - The piece location row.
+     * @param player - The color of the piece.
+     * @param resId - The ID of his image.
+     */
     public Knight(int col, int row, ChessPlayer player, int resId) {
         super(col, row, player,ChessRank.KNIGHT, resId);
     }
+
+    /**
+     * set green_square to be all the optional movements of the knight.
+     */
     public void moveOption(){
         int i = 0;
         int tempCol = col-1,tempRow = row+2;
