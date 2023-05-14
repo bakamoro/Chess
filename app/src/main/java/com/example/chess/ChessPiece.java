5ef16e0b62b43144d8a66e9d027fe586b7ff6cc2
Bakamoro <binyaminkenig@gmail.com>
2023-05-14 13:06:01 +0300

    adding document to code

--- app/src/main/java/com/example/chess/ChessPiece.java
+++ app/src/main/java/com/example/chess/ChessPiece.java
@@ -3,9 +3,20 @@ package com.example.chess;
 import static com.example.chess.ChessDelegate.pieceBox;
 
 public class ChessPiece  {
-    int col, row, resId;
-    ChessPlayer player;
-    ChessRank rank;
+    int col//The piece location column.
+            , row//The piece location row.
+            , resId;//The ID of his image.
+    ChessPlayer player;//The color of the piece.
+    ChessRank rank;//the grade of the piece.
+
+    /**
+     * The constructor
+     * @param col - The piece location column.
+     * @param row - The piece location row.
+     * @param player - The color of the piece.
+     * @param rank - the grade of the piece.
+     * @param resId - The ID of his image.
+     */
     public ChessPiece(int col , int row , ChessPlayer player, ChessRank rank, int resId){
         this.col = col;
         this.row = row;
@@ -13,9 +24,23 @@ public class ChessPiece  {
         this.rank = rank;
         this.resId = resId;
     }
+
+    /**
+     * This function checks if the given location is legal.
+     * @param col - The location's column.
+     * @param row - The location's row.
+     * @return true if the location is legal and false if it isn't.
+     */
     protected boolean col_row_legal(int col, int row) {
         return  (col >= 1 && col <= 8 && row >= 1 && row <= 8);
     }
+
+    /**
+     * This function check if there is a piece in the given location and if there is she return it.
+     * @param col - The location's column.
+     * @param row - The location's row.
+     * @return
+     */
     public ChessPiece pieceAt(int col, int row){
         if(col<1 || row<1 || col > 8 || row > 8) return null;
         for(int i = 0 ; i < 32;i++){
