32c2ecd2ccf6e32f1132a78927d48a7fe6fb2d34
Bakamoro <binyaminkenig@gmail.com>
2023-06-18 12:50:40 +0300

    fix bug with black pawn

--- app/src/main/java/com/example/chess/Pawn.java
+++ app/src/main/java/com/example/chess/Pawn.java
@@ -21,25 +21,25 @@ public class Pawn extends ChessPiece{
      */
     public void moveOption(String color){
         int i = 0;
-        if(color.equals("black")){
-            player = ChessPlayer.WHITE;
-        }
-        if(player == ChessPlayer.BLACK){
-            if(row == 7) {
-                if (pieceAt(col, row - 1) == null) {
-                    green_square[i++] = new Green_Square(col, row - 1);
-                    if (pieceAt(col, row - 2) == null) {
-                        green_square[i++] = new Green_Square(col, row - 2);
-                    }
-                }
-            }
-            else  if (pieceAt(col, row - 1) == null) {
-                green_square[i++] = new Green_Square(col, row - 1);
-            }
-            if (pieceAt(col-1,row-1)!=null && pieceAt(col-1,row-1).player != player)green_square[i++] = new Green_Square(col-1,row-1);
-            if (pieceAt(col+1,row-1)!=null && pieceAt(col+1,row-1).player != player)green_square[i++] = new Green_Square(col+1,row-1);
-        }
-        else {
+//        if(color.equals("black")){
+//            player = ChessPlayer.WHITE;
+//        }
+//        if(player == ChessPlayer.BLACK){
+//            if(row == 7) {
+//                if (pieceAt(col, row - 1) == null) {
+//                    green_square[i++] = new Green_Square(col, row - 1);
+//                    if (pieceAt(col, row - 2) == null) {
+//                        green_square[i++] = new Green_Square(col, row - 2);
+//                    }
+//                }
+//            }
+//            else  if (pieceAt(col, row - 1) == null) {
+//                green_square[i++] = new Green_Square(col, row - 1);
+//            }
+//            if (pieceAt(col-1,row-1)!=null && pieceAt(col-1,row-1).player != player)green_square[i++] = new Green_Square(col-1,row-1);
+//            if (pieceAt(col+1,row-1)!=null && pieceAt(col+1,row-1).player != player)green_square[i++] = new Green_Square(col+1,row-1);
+//        }
+//        else {
             if(row == 2) {
                 if (pieceAt(col, row + 1) == null) {
                     green_square[i++] = new Green_Square(col, row + 1);
@@ -55,6 +55,6 @@ public class Pawn extends ChessPiece{
                 green_square[i++] = new Green_Square(col-1,row+1);
             if (pieceAt(col+1,row+1)!=null && pieceAt(col+1,row+1).player != player)
                 green_square[i++] = new Green_Square(col+1,row+1);
-        }
+//        }
     }
 }
